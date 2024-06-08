42d5086c4dd3ef8412a061d760513d67b2dc2532
josemavarez11 <josemmr311@gmail.com>
2024-06-07 21:49:18 -0400

    updating allow cors origin in settings.py

--- server/settings.py
+++ server/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware'
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -78,6 +79,7 @@ DATABASES = {
     }
 }
 
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Password validation
